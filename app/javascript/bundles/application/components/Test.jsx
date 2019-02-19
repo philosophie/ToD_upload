428b2f6d7746245b64a13ee8a51c819fd6419bff
Nour <nour@philosophie.is>
2019-02-19 10:53:25 -0500

    WIP implement front-end

--- app/javascript/bundles/application/components/Test.jsx
+++ app/javascript/bundles/application/components/Test.jsx
@@ -1,11 +1,67 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
+import ReactDataSheet from "react-datasheet";
 
 export default class Test extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: this.setData()
+    };
+  }
+
+  setData = () => {
+    const table = [];
+    const { data, numberOfSampleColumns } = this.props;
+    data.forEach(function(element) {
+      if (data.indexOf(element) === 0) {
+        const firstRow = [];
+        element.forEach(function(cell) {
+          if (
+            element.indexOf(cell) >= 0 &&
+            element.indexOf(cell) < numberOfSampleColumns
+          ) {
+            firstRow.push(
+              Object.assign(
+                {},
+                { value: cell, className: "sample-data-column-header" }
+              )
+            );
+          } else {
+            firstRow.push(
+              Object.assign(
+                {},
+                { value: cell, className: "test-data-column-header" }
+              )
+            );
+          }
+        });
+        table.push(firstRow);
+      } else {
+        const row = [];
+        element.forEach(function(cell) {
+          row.push(Object.assign({}, { value: cell }));
+        });
+        table.push(row);
+      }
+    });
+    return table;
+  };
   render() {
     return (
       <div>
-        <h3>Test</h3>
+        <h1>{this.props.pageTitle}</h1>
+        <ReactDataSheet
+          data={this.state.data}
+          valueRenderer={cell => cell.value}
+          onCellsChanged={changes => {
+            const data = this.state.data.map(row => [...row]);
+            changes.forEach(({ cell, row, col, value }) => {
+              data[row][col] = { ...data[row][col], value };
+            });
+            this.setState({ data });
+          }}
+        />
       </div>
     );
   }
